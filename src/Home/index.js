ef34ae4cfdc2abb56e897b7aacba4df583844be5
liushuai <liushuai@teddymobile.cn>
2020-05-20 23:10:08 +0800

    m

--- src/Home/index.js
+++ src/Home/index.js
@@ -20,7 +20,7 @@ export default Custom_page({
   },
   async getData() {
     const $appDef = this.$app.$def
-    const {data} = await $appDef.$http.get(`/zaoan/index?key=${$appDef.key}`)
+    const {data} = await $appDef.$http.get(`/proverb/index?key=${$appDef.key}`)
     if(data.code === 200) {
       this.newsList = data.newslist
     }
@@ -29,7 +29,7 @@ export default Custom_page({
   },
   longPress(item, e) {
     clipboard.set({
-      text: `${item.content}`,
+      text: `${item.front}\n${item.behind}`,
       success () {
         prompt.showToast({
           message: '复制成功'
--- src/statistics.config.js
+++ src/statistics.config.js
@@ -1,3 +1,3 @@
 export default {
-  'app_key' : '74fbd1b699f451eaa1dd0bac650e63f3'   //请在此行填写统计平台获取的 app_kye
+  'app_key' : '3bfa177099f851ea8dfc35d50b0dac70'   //请在此行填写统计平台获取的 app_kye
 }
